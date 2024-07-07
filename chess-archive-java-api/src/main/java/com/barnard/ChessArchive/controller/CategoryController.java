d5e7d74eb38fbfeb60987e674017752a8da1bdcd
William Barnard <barnardwill@gmail.com>
2024-07-06 19:57:17 -0500

    added source and category controllers

--- chess-archive-java-api/src/main/java/com/barnard/ChessArchive/controller/CategoryController.java
+++ chess-archive-java-api/src/main/java/com/barnard/ChessArchive/controller/CategoryController.java
@@ -1,4 +1,35 @@
 package com.barnard.ChessArchive.controller;
 
+import com.barnard.ChessArchive.dao.CategoryDao;
+import com.barnard.ChessArchive.model.OpeningCategory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin
+@RequestMapping(path = "/category")
 public class CategoryController {
+
+    @Autowired
+    private CategoryDao categoryDao;
+
+    @GetMapping(path = "/{categoryId}")
+    public OpeningCategory getCategory(@PathVariable int categoryId) {
+        return categoryDao.getCategory(categoryId);
+    }
+
+    @PostMapping(path = "")
+    public OpeningCategory createCategory(@RequestBody OpeningCategory openingCategory) {
+        return categoryDao.createCategory(openingCategory);
+    }
+
+    @PutMapping(path = "")
+    public OpeningCategory updateCategory(@RequestBody OpeningCategory openingCategory) {
+        return categoryDao.updateCategory(openingCategory);
+    }
+
+    @DeleteMapping(path = "/{categoryId}")
+    public void deleteCategory(@PathVariable int categoryId) {
+        categoryDao.deleteCategory(categoryId);
+    }
 }
--- chess-archive-java-api/src/main/java/com/barnard/ChessArchive/controller/SourceController.java
+++ chess-archive-java-api/src/main/java/com/barnard/ChessArchive/controller/SourceController.java
@@ -1,4 +1,35 @@
 package com.barnard.ChessArchive.controller;
 
+import com.barnard.ChessArchive.dao.SourceDao;
+import com.barnard.ChessArchive.model.Source;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin
+@RequestMapping(path = "/source")
 public class SourceController {
+
+    @Autowired
+    private SourceDao sourceDao;
+
+    @GetMapping(path = "/{sourceId}")
+    public Source getSource(@PathVariable int sourceId) {
+        return sourceDao.getSource(sourceId);
+    }
+
+    @PostMapping(path = "")
+    public Source createSource(@RequestBody Source source) {
+        return sourceDao.createSource(source);
+    }
+
+    @PutMapping(path = "")
+    public Source updateSource(@RequestBody Source source) {
+        return sourceDao.updateSource(source);
+    }
+
+    @DeleteMapping(path = "/{sourceId}")
+    public void deleteSource(@PathVariable int sourceId) {
+        sourceDao.deleteSource(sourceId);
+    }
 }
